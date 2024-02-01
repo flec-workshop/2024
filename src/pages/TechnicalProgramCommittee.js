3ab36d3fbc606b38252cf3e53c51d6629a564d0a
Alessio Catalfamo <alessiocatalfamo@gmail.com>
2024-02-01 11:37:07 +0100

    fixed order tpc

--- src/pages/TechnicalProgramCommittee.js
+++ src/pages/TechnicalProgramCommittee.js
@@ -10,15 +10,20 @@ const SteeringCommittee = () => {
         <hr />
         <table>
           <tbody>
-            {/* TBD */}
             <tr>
-              <td>Boris Kraychev, GATE Institute, Bulgaria</td>
+              <td>Sabtain Ahmad, TU Wien, Austria</td>
+            </tr>
+            <tr>
+              <td>Fahed Alkhabbas, Malmö University, Sweden</td>
+            </tr>
+            <tr>
+              <td>Sadi Alawadi, Blekinge Tekniska Högskola (BTH), Sweden</td>
+            </tr>
+            <tr>
+              <td>Addi Ait-Mlouk, University of Skövde, Sweden</td>
             </tr>
             <tr>
               <td>Rodrigo Calheiros, Western Sydney University, Australia</td>
-            </tr>
-            <tr>
-              <td>Reza Jabbarpour, Swinburne University, Australia</td>
             </tr>
             <tr>
               <td>Marco Garofalo, University of Pisa, Italy</td>
@@ -27,31 +32,25 @@ const SteeringCommittee = () => {
               <td>Mark Adrian Gambito, University of Pisa, Italy</td>
             </tr>
             <tr>
-              <td>Sabtain Ahmad, TU Wien, Austria</td>
+              <td>Reza Jabbarpour, Swinburne University, Australia</td>
             </tr>
             <tr>
-              <td>Omar Abdel Wahab, Polytechnique de Montréal</td>
-            </tr>
-            <tr>
-              <td>SADI ALAWADI, Blekinge Tekniska Högskola (BTH), Sweden</td>
-            </tr>
-            <tr>
-              <td>Fahed Alkhabbas, Malmö University, Sweden</td>
-            </tr>
-            <tr>
-              <td>Addi Ait-Mlouk, University of Skövde, Sweden</td>
-            </tr>
-            <tr>
-              <td>Florian van Daalen, Maastricht University</td>
+              <td>Boris Kraychev, GATE Institute, Bulgaria</td>
             </tr>
             <tr>
               <td>William Lindskog, Technical University of Munich</td>
             </tr>
             <tr>
+              <td>Posco Tso, Loughborough University, UK</td>
+            </tr>
+            <tr>
               <td>Mehreen Tahir, Dublin City University</td>
             </tr>
             <tr>
-              <td>Posco Tso, Loughborough University, UK</td>
+              <td>Florian van Daalen, Maastricht University</td>
+            </tr>
+            <tr>
+              <td>Omar Abdel Wahab, Polytechnique de Montréal</td>
             </tr>
           </tbody>
         </table>
