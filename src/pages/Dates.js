f5d627ff00198d8d4154b00d225956585a603312
Alessio Catalfamo <alessiocatalfamo@gmail.com>
2024-03-12 17:33:30 +0100

    extension

--- src/pages/Dates.js
+++ src/pages/Dates.js
@@ -9,7 +9,7 @@ const Dates = () => {
                 <h1>Important Dates</h1>
                 <hr />
                 <p><strong>Timezone</strong><br/>Anywhere in the world!</p>
-                <p> <strong>Paper Submissions Due</strong><br/>March 9, 2024</p>
+                <p> <strong>Paper Submissions Due</strong><br/>March 21, 2024</p>
                 <p> <strong> Notification of Acceptance</strong><br/>April 7, 2024  </p>
                 <p> <strong>Camera-Ready Due</strong><br/>April 16, 2024</p>
                 <p> <strong>Workshop Days</strong><br/>June 3-4, 2024</p>
