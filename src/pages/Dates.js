8cf38f25e9f978c246179ec4e8865618477d5d62
Alessio Catalfamo <alessiocatalfamo@gmail.com>
2024-03-22 10:00:42 +0100

    extended deadline

--- src/pages/Dates.js
+++ src/pages/Dates.js
@@ -9,9 +9,9 @@ const Dates = () => {
                 <h1>Important Dates</h1>
                 <hr />
                 <p><strong>Timezone</strong><br/>Anywhere in the world!</p>
-                <p> <strong>Paper Submissions Due</strong><br/><del>March 21, 2024</del> April 1, 2024</p>
-                <p> <strong> Notification of Acceptance</strong><br/>April 7, 2024  </p>
-                <p> <strong>Camera-Ready Due</strong><br/>April 16, 2024</p>
+                <p> <strong>Paper Submissions Due</strong><br/><del>March 21, 2024</del> April 1, 2024 (FIRM)</p>
+                <p> <strong> Notification of Acceptance</strong><br/>April 9, 2024  </p>
+                <p> <strong>Camera-Ready Due</strong><br/>April 17, 2024</p>
                 <p> <strong>Workshop Days</strong><br/>June 3-4, 2024</p>
             </Content>
         </PageWrapper>
